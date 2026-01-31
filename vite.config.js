53139acac35b15383b0d59c9c7cf5c25c6394e99
VarelaG <varelag1999@gmail.com>
2026-01-31 19:26:10 -0300

    Update vite base configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
-  base: '/Portafolio/', // <--- AGREGA ESTA LÍNEA VITAL
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
